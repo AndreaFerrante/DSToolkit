7ca9f28711b2f93cfbc344633fdf29ca0c2f071b
Andrea Ferrante <andreaferrante1986@gmail.com>
2021-12-29 22:40:21 +0100

    Added selection sort from scratch

---
+++ DSToolkit/sorting/selection_sort.py
@@ -0,0 +1,17 @@
+def selection_sort(arr):
+
+	n     = len(arr)
+
+	for i in range(n):
+		start = i
+		for j in range(i + 1, n):
+			if arr[j] < arr[start]:
+				start = j
+
+		arr[i], arr[start] = arr[start], arr[i]
+
+	return arr
+
+
+arr = [13,14,7,6,4,8,7,11,1,12]
+print(selection_sort(arr))
