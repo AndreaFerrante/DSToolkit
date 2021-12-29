b0ba52601a71c59a93c4797499caaea067e2dcff
Andrea Ferrante <andreaferrante1986@gmail.com>
2021-12-29 21:38:19 +0100

    Added counting, heap and insertion sort

--- DSToolkit/sorting/insertion_sort.py
+++ DSToolkit/sorting/insertion_sort.py
@@ -1,30 +1,30 @@
 def insertion_sort(arr):
 
-	for i in range(1, len(arr)):
+    for i in range(1, len(arr)):
 
-		key = arr[i]
-		j   = i - 1
+        key = arr[i]
+        j   = i - 1
 
-		while j >= 0 and arr[j] > key:
+        while j >= 0 and arr[j] > key:
 
-			arr[j + 1] = arr[j]
-			j -= 1
+            arr[j + 1] = arr[j]
+            j -= 1
 
-		arr[j + 1] = key
+        arr[j + 1] = key
 
-	return arr
+    return arr
 
 
 def test_insertion_sort():
 
-	arr = [3,4,1,2,6,9,7,8]
+    arr = [3,4,1,2,6,9,7,8]
 
-	for i in range(len(arr)):
-		print('Unsorted values in arr are: ', arr[i])
+    for i in range(len(arr)):
+        print('Unsorted values in arr are: ', arr[i])
 
-	insertion_sort(arr)
-	for j in range(len(arr)):
-		print('Sorted values in arr are: ', arr[j])
+    insertion_sort(arr)
+    for j in range(len(arr)):
+        print('Sorted values in arr are: ', arr[j])
 
 
 test_insertion_sort()
