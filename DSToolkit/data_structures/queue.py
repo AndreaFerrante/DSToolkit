376dab00c2e9ed76539ef93839b6c23f733cfc93
Andrea Ferrante <andreaferrante1986@gmail.com>
2022-10-21 19:51:54 +0200

    Added logic to queue data structure

--- DSToolkit/data_structures/queue.py
+++ DSToolkit/data_structures/queue.py
@@ -2,7 +2,8 @@
 class Queue(object):
 
 	'''
-	Queues are list that insert data using FIFO extraction tecnique: the first element inserted is the one that is 
+	Queues are lists that insert data using FIFO manner: the first element inserted is the one that is
+	extracted first (first in first out). The opposite data structure is the stack (LIFO, last in first out)
 	'''
 
 	def __init__(self):
@@ -17,3 +18,17 @@ class Queue(object):
 
 		return self.queue.pop(0)
 
+	def print_elements(self):
+		for el in self.queue:
+			print(el)
+
+
+# Driver code...
+q = Queue()
+q.insert(10)
+q.insert(111)
+q.insert('element')
+
+q.print_elements()
+q.retrieve()
+q.print_elements()
